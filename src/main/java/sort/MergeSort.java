4cb86b07f04f7a9f0c81813845e1688ebfba7322
diwa <543667537@qq.com>
2018-07-14 16:54:02 +0800

    add merge sort

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -1,32 +1,51 @@
 package sort;
 
+import common.PrintUtil;
+
 public class MergeSort {
     private static int[] arr = new int[]{1, 2, 3, 435, 56, 345, 3, 56, 7, 657, 3, 5};
 
     public static void main(String[] args) {
 
+        mSort(arr, 0, arr.length - 1);
+
+        PrintUtil.printArray("after", arr);
     }
 
-    private static void mergeSort(int[] arr, int left, int right) {
-        if (left >= right) {
-            return;
-        }
+    public static void mSort(int arr[], int left, int right) {
+        if (left >= right) return;
 
         int mid = (left + right) / 2;
 
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
+        mSort(arr, left, mid);
+        mSort(arr, mid + 1, right);
 
         merge(arr, left, mid, right);
     }
 
-    private static void merge(int[] arr, int left, int mid, int right) {
-        int temp[] = new int[right - left + 1];
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int oriLeft = left;
 
-        int leftPoint =0;
-        int rightPoint =0;
+        int first = left;
+        int second = mid + 1;
 
+        int[] tmp = new int[right - left + 1];
 
+        int cnt = 0;
+        while (first <= mid && second <= right) {
+            tmp[cnt++] = arr[first] < arr[second] ? arr[first++] : arr[second++];
+        }
 
+        while (first <= mid) {
+            tmp[cnt++] = arr[first++];
+        }
+
+        while (second <= right) {
+            tmp[cnt++] = arr[second++];
+        }
+
+        for (int i = 0; i < tmp.length; i++) {
+            arr[oriLeft++] = tmp[i];
+        }
     }
 }
